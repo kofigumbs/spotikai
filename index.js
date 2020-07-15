c742b7b3267ca9775b1431790b3078290c80e82b
Kofi Gumbs <h.kofigumbs@gmail.com>
2020-07-15 17:30:26 -0400

    Password-protect the stream

--- index.js
+++ index.js
@@ -2,35 +2,42 @@ const fs = require("fs");
 const http = require("http");
 const lame = require("@suldashi/lame");
 const { spawn } = require("child_process");
-const { PORT, SPOTIFY_USERNAME, SPOTIFY_PASSWORD } = process.env;
+const { PORT, STREAM_PASSWORD, SPOTIFY_USERNAME, SPOTIFY_PASSWORD } = process.env;
+
+const static = (response, path, contentType) => {
+  response.writeHead(200, { "Content-Type": contentType });
+  fs.createReadStream(`static/${path}`).pipe(response);
+};
+
+const notFound = response => {
+  response.writeHead(404);
+  response.end();
+};
+
+const audio = response => {
+  const flags = [
+    "--name", "SpotiKai",
+    "--backend", "pipe",
+    "--initial-volume", "100",
+    "--username", SPOTIFY_USERNAME, "--password", SPOTIFY_PASSWORD,
+  ];
+  response.writeHead(200, { "Content-Type": "audio/mpeg" });
+  spawn("librespot", flags, { stdio: [ "ignore", "pipe", "inherit" ] })
+    .stdout.pipe(new lame.Encoder()).pipe(response);
+};
 
 const server = http.createServer((request, response) => {
   switch (request.url) {
     case "/":
-      response.writeHead(200, { "Content-Type": "text/html" });
-      fs.createReadStream("static/index.html").pipe(response);
-      break;
-
+      return static(response, "index.html", "text/html");
     case "/manifest.webapp":
-      response.writeHead(200, { "Content-Type": "application/x-web-app-manifest+json" });
-      fs.createReadStream("static/manifest.webapp").pipe(response);
-      break;
-
+      return static(response, "manifest.webapp.json", "application/x-web-app-manifest+json");
     case "/audio.mp3":
-      const flags = [
-        "--name", "SpotiKai",
-        "--backend", "pipe",
-        "--initial-volume", "100",
-        "--username", SPOTIFY_USERNAME, "--password", SPOTIFY_PASSWORD,
-      ];
-      response.writeHead(200, { "Content-Type": "audio/mpeg" });
-      spawn("librespot", flags, { stdio: [ "ignore", "pipe", "inherit" ] })
-        .stdout.pipe(new lame.Encoder()).pipe(response);
-      break;
-
+      return request.headers.cookie === `password=${STREAM_PASSWORD}`
+        ? audio(response)
+        : notFound(response);
     default:
-      response.writeHead(404, {});
-      response.end();
+      return notFound(response);
   }
 });
 
